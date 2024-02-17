0868b211d8a67cb2103e7b2cc5bae7d052998164
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-02-17 15:49:32 -0600

    progress

--- text_editor.py
+++ text_editor.py
@@ -4,7 +4,13 @@ import tkinter as tk
 def main():
     window = tk.Tk()
     window.title("Text Editor with Python")
+    window.rowconfigure(0, minsize=400)
+    window.columnconfigure(1, minsize=500)
 
+
+    text_edit = tk.Text(window, font="Helvetica 18")
+    text_edit.grid(row=0, column=1)
+    
     window.mainloop()
 
 main()
