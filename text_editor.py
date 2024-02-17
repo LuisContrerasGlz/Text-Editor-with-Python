8ce5116370040efc9bc384038325ce1b3032336c
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-02-17 15:57:27 -0600

    progress

--- text_editor.py
+++ text_editor.py
@@ -10,7 +10,15 @@ def main():
 
     text_edit = tk.Text(window, font="Helvetica 18")
     text_edit.grid(row=0, column=1)
-    
+
+    frame = tk.Frame(window, relief=tk.RAISED, bd=2)
+    save_button = tk.Button(frame, text= "Save")
+    open_button = tk.Button(frame, text= "Open")
+
+    save_button.grid(row=0, column=0)
+    open_button.grid(row=1, column=0)
+    frame.grid(row=0, column=0)
+
     window.mainloop()
 
 main()
