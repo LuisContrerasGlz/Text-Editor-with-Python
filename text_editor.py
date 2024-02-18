4aa1495bbd9de42ef8538b592ee9dca8a20f2458
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-02-18 14:49:02 -0600

    progress

--- text_editor.py
+++ text_editor.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter.filedialog import askopenfile, askopenfilename, asksaveasfile
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 
 def open_file(window, text_edit):
@@ -14,8 +14,16 @@ def open_file(window, text_edit):
         text_edit.insert(tk.END, content)
     window.title(f"Open File: {filepath}")
  
-def save_file():
-    pass
+def save_file(window, text_edit):
+    filepath = asksaveasfilename(filetypes=[("Text Files", "*.txt")])
+
+    if not filepath:
+        return
+    
+    with open(filepath, "w") as f:
+        content = text_edit.get(1.0, tk.END)
+        f.write(content)
+    window.title(f"Open File: {filepath}")
 
 def main():
     window = tk.Tk()
@@ -28,7 +36,7 @@ def main():
     text_edit.grid(row=0, column=1)
 
     frame = tk.Frame(window, relief=tk.RAISED, bd=2)
-    save_button = tk.Button(frame, text= "Save")
+    save_button = tk.Button(frame, text= "Save", command=lambda: save_file(window, text_edit))
     open_button = tk.Button(frame, text= "Open", command=lambda: open_file(window, text_edit))
 
     save_button.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
